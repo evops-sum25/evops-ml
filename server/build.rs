735232ce5729c366849ea7bbb68e424095e5db15
Egor Pustovoytenko <puseg2006@gmail.com>
2025-06-18 13:49:13 +0300

    fix(ml-server): remove file descriptor generation for protoc (no reflection)

--- server/build.rs
+++ server/build.rs
@@ -1,13 +1,7 @@
-use std::env;
-use std::path::PathBuf;
-
 fn main() -> eyre::Result<()> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
-    tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("ml-descriptor.bin"))
-        .compile_protos(
-            &["../server-ext/proto/evops/ml/v1/ml.proto"],
-            &["../server-ext/proto/"],
-        )?;
+    tonic_build::configure().compile_protos(
+        &["../server-ext/proto/evops/ml/v1/ml.proto"],
+        &["../server-ext/proto/"],
+    )?;
     Ok(())
 }
